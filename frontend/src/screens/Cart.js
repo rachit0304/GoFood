6e5b8f6836bb989e6a57d5a6d289c4ba9bf01d0b
rachit0304 <98216013+rachit0304@users.noreply.github.com>
2024-01-24 18:21:19 +0530

    Update Cart.js

--- frontend/src/screens/Cart.js
+++ frontend/src/screens/Cart.js
@@ -19,7 +19,7 @@ export default function Cart() {
   const handleCheckOut = async () => {
     let userEmail = localStorage.getItem("userEmail");
     // console.log(data,localStorage.getItem("userEmail"),new Date())
-    let response = await fetch("http://localhost:5000/api/myorders", {
+    let response = await fetch("https://go-food-server.onrender.com/api/myorders", {
       // credentials: 'include',
       // Origin:"http://localhost:3000/login",
       method: 'POST',
