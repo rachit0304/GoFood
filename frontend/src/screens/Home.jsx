f064be5a931baeed938897bed17c7fa877ff9ee7
rachit0304 <98216013+rachit0304@users.noreply.github.com>
2024-01-24 16:56:13 +0530

    Update Home.jsx

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   let [fooditem, setFooditem] = useState([]);
 
   const loadData = async () => {
-    let response = await fetch("http://localhost:5000/api/foodData", {
+    let response = await fetch("https://go-food-server.vercel.app/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
