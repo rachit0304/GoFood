fb3feeba4bdc031f6ed75240a0a605d2dd4e3834
rachit0304 <98216013+rachit0304@users.noreply.github.com>
2024-01-24 18:13:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ mongoDB();
 // })    
 
 app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "https://go-food-front.vercel.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
