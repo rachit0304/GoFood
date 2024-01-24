70457228c1ccd139e006c43700a79df99a9d7861
rachit0304 <98216013+rachit0304@users.noreply.github.com>
2024-01-24 17:06:14 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,35 @@
-import './App.css';
-import Home from './screens/Home';
-import Login from './screens/Login';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
+// import './App.css';
+// import Home from './screens/Home';
+// import Login from './screens/Login';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
   
-} from "react-router-dom";
-import Signup from './screens/Signup';
-import { CartProvider } from './components/ContextReducer';
-import MyOrders from './screens/MyOrders';
-import Cart from './screens/Cart'; 
+// } from "react-router-dom";
+// import Signup from './screens/Signup';
+// import { CartProvider } from './components/ContextReducer';
+// import MyOrders from './screens/MyOrders';
+// import Cart from './screens/Cart'; 
 
-function App() {
-  return (
-  <CartProvider>
-   <Router>
+// function App() {
+//   return (
+//   <CartProvider>
+//    <Router>
     
-    <div> 
-      <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route exact path="/login" element={<Login/>} />
-        <Route exact path="/createuser" element={<Signup/>} />
-        <Route exact path="/myorders" element={<MyOrders/>} />
-        <Route exact path="/cart" element={<Cart/>} />
-      </Routes>
-     </div>
+//     <div> 
+//       <Routes>
+//         <Route exact path="/" element={<Home/>} />
+//         <Route exact path="/login" element={<Login/>} />
+//         <Route exact path="/createuser" element={<Signup/>} />
+//         <Route exact path="/myorders" element={<MyOrders/>} />
+//         <Route exact path="/cart" element={<Cart/>} />
+//       </Routes>
+//      </div>
    
-   </Router>
-   </CartProvider>
-  );
-}
+//    </Router>
+//    </CartProvider>
+//   );
+// }
 
-export default App;
+// export default App;
