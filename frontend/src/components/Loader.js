bfef79a6c5c1baaeb241608c16e025bc4bd23a76
rachit0304 <98216013+rachit0304@users.noreply.github.com>
2024-02-01 14:55:29 +0530

    Update Loader.js

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -4,7 +4,7 @@ const Loader = () => {
   return (
     <div>
       <div class="d-flex justify-content-center">
-        <div class="spinner-border spinner" style={{width: "5rem" , height: "5rem"}} role="status">
+        <div class="spinner-border" style={{width: "5rem" , height: "5rem"}} role="status">
         </div>
         </div>
     </div>
