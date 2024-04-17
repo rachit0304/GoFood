681b78e63820974420fc17855b2d84aa704e9b85
Rachit Sharma <98216013+rachit0304@users.noreply.github.com>
2024-04-18 00:29:50 +0530

    Create Caraousel.js

---
+++ frontend/src/components/Caraousel.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Caraousel({search, setSearch}) {
+  return (
+    <div>
+       <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-inner" id='carousel'>
+                    <div className="carousel-caption" style={{zIndex: "10"}}>
+                    <div className="d-flex justify-content-center">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={search} onChange={(e)=>{setSearch(e.target.value)}} />
+                    </div>
+                    </div>
+                <div className="carousel-item active imgage">
+                <img src="https://source.unsplash.com/random/600x400/?burger" className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                <img src="https://source.unsplash.com/random/600x400/?sandwhich" className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                <img src="https://source.unsplash.com/random/600x400/?pizza" className="d-block w-100" alt="..."/>
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+            </div>
+    </div>
+  )
+}
