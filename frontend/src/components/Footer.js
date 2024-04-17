789bb0cb3bc0864a51d07b76c7974dff09e6a8e1
Rachit Sharma <98216013+rachit0304@users.noreply.github.com>
2024-04-18 00:00:05 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,11 +7,11 @@ export default function Footer() {
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
                 <div className="col-md-4 d-flex align-items-center">
                 
-                <span className="text-muted">Developer - Rachit Sharma</span>
+                <span className="text-muted" style={{marginLeft: '20px'}} >Developer - Rachit Sharma</span>
                 </div>
                 <span className="text-muted"> 2023 GoFood </span> 
                 <br />
-                <span className="text-muted">email - rachitsharma2803@gmail.com</span>
+                <span className="text-muted" style={{marginRight: '20px'}} >email - rachitsharma2803@gmail.com</span>
                 
         </footer>
     </div>
