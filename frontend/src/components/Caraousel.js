f9bf2b7f67808fb102b0f6e6b0414e713236a272
Rachit Sharma <98216013+rachit0304@users.noreply.github.com>
2024-06-27 15:03:41 +0530

    Update Caraousel.js

--- frontend/src/components/Caraousel.js
+++ frontend/src/components/Caraousel.js
@@ -10,13 +10,13 @@ export default function Caraousel() {
                     </div>
                     </div>
                 <div className="carousel-item active imgage">
-                <img src="https://source.unsplash.com/random/600x400/?burger" className="d-block w-100" alt="Burger Image"/>
+                <img src="https://img.freepik.com/free-vector/gradient-indian-restaurant-facebook-cover_23-2149429439.jpg?w=1380&t=st=1719480001~exp=1719480601~hmac=de040fa99b750f7bc7c742c0828ff1da1f3fc4de63496b6ba9fa8cf0a75286f7" className="d-block w-100" alt="Burger Image"/>
                 </div>
                 <div className="carousel-item">
-                <img src="https://source.unsplash.com/random/600x400/?sandwhich" className="d-block w-100" alt="Sandwhich Image"/>
+                <img src="https://img.freepik.com/free-photo/flat-lay-healthy-vegetarian-dinner-table-setting-sandwiches-with-tomato-cucumber-avocado-strawberry-herbs-olives-snacks-clean-eating-vegan-food_2829-17047.jpg?w=1380&t=st=1719479945~exp=1719480545~hmac=f3b800dc3adcf6b1e48b37aab56e11e4d22811710d23e1d63d7a613d613b29b3" className="d-block w-100" alt="Sandwhich Image"/>
                 </div>
                 <div className="carousel-item">
-                <img src="https://source.unsplash.com/random/600x400/?pizza" className="d-block w-100" alt="Pizza Image"/>
+                <img src="https://img.freepik.com/free-photo/fresh-gourmet-meal-beef-taco-salad-plate-generated-by-ai_188544-13382.jpg?w=1060&t=st=1719480804~exp=1719481404~hmac=1189b7bdb3631c34b4bcc628cb1c32201fe71aa41cba93cff40766a9084a9618" className="d-block w-100" alt="Pizza Image"/>
                 </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev">
