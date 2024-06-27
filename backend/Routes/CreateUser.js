ccd36e908900a77568c4780f804040b7b93c9ff8
Rachit Sharma <98216013+rachit0304@users.noreply.github.com>
2024-06-27 15:24:44 +0530

    Update CreateUser.js

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -1,68 +1,92 @@
 const express = require('express');
 const router = express.Router();
-const user = require('../models/User');
-const { query, validationResult } = require('express-validator');
+const User = require('../models/User');
+const { query, body, validationResult } = require('express-validator');
 
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const jwtSecret = process.env.REACT_APP_JWT_SECRET;
 
 router.post('/createuser',
-// [query('name','name cannot be empty').notEmpty(),
-// query('email', 'not a valid email').isEmail(),
-// query('password', 'min length should be 5').isLength({min:5})],
+[
+query('name','name cannot be empty').notEmpty(),
+query('email', 'not a valid email').isEmail(),
+query('password', 'min length should be 5').isLength({min:5})
+],
 async (req,res)=>{
 
-//     const errors = validationResult(req);
-//   if (!(errors.isEmpty())) {
-//     res.send({ errors: errors.array() });
-
-//   }
+    const errors = validationResult(req);
+  if (!(errors.isEmpty())) {
+     return res.status(400).json({ success, errors: errors.array() })
+  }
 
     let salt = await bcrypt.genSalt(10);
     let secPass =await bcrypt.hash(req.body.password , salt)
 
     try {
-        await user.create({
+        await User.create({
             name: req.body.name,
             address: req.body.address,
             email: req.body.email,
             password: secPass
+        }).then(user => {
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(data, jwtSecret);
+            success = true
+            res.json({ success, authToken })
         })
-      
-        res.json({success:true})
-
-    } catch (error) {   
-        console.log(error);
-        res.json({success:false})
+            .catch(err => {
+                console.log(err);
+                res.json({ error: "Please enter a unique value." })
+            })
+    } catch (error) {
+        console.error(error.message)
     }
 })
 
 
 router.post('/loginuser',
+            [
+    body('email', "Enter a Valid Email").isEmail(),
+    body('password', "Password cannot be blank").exists(),
+],
 async (req,res)=>{  
-    let email = req.body.email;
+    
     let success = false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
     }
+    
+    let email = req.body.email;
+    let password = req.body.password;
+
 
     try {
-        let userData =  await user.findOne({email})
+        let userData =  await User.findOne({email})
         
         if(!userData){
             return res.status(400).json({ success, error: "No user found" });
         }
 
-        let pwdCompare = bcrypt.compare(req.body.password , userData.password)
+        let pwdCompare = await bcrypt.compare(password , userData.password)
 
         if(!pwdCompare){
             return res.status(400).json({ success, error: "Enter valid credentials" });
         }
-
-        const authToken = jwt.sign({id: userData._id}, jwtSecret)
-        res.json({success:true , authToken:authToken})
+        
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        success = true;
+        const authToken = jwt.sign(data, jwtSecret);
+        res.json({ success, authToken })
 
     } catch (error) {   
         console.log(error.message);
