5c8bd75b027c3abfb3222f7c203853f6001304d4
Rachit Sharma <98216013+rachit0304@users.noreply.github.com>
2024-07-03 20:39:13 +0530

    Update Home.jsx

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
 
   const loadData = async () => {
     setFetching(true);
-    let response = await fetch("http://localhost:5000/api/foodData", {
+    let response = await fetch("https://go-food-server.onrender.com/api/foodData", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
