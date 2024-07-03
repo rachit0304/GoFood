359d88547cbda89cd07d395c88f6d7b007db5e04
Rachit Sharma <98216013+rachit0304@users.noreply.github.com>
2024-07-03 16:59:10 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,15 +3,15 @@ import {Link} from 'react-router-dom'
 
 export default function Footer() {
   return (
-    <div>
+    <div className='container'>
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
                 <div className="col-md-4 d-flex align-items-center">
                 
-                <span className="text-muted" style={{marginLeft: '20px'}} >Developer - Rachit Sharma</span>
+                <span className="text-muted" style={{marginLeft: '20px'}}>Developer - Rachit Sharma</span>
                 </div>
                 <span className="text-muted"> 2023 GoFood </span> 
                 <br />
-                <span className="text-muted" style={{marginRight: '20px'}} >email - rachitsharma2803@gmail.com</span>
+                <span className="text-muted" style={{marginRight: '20px'}}>email - rachitsharma2803@gmail.com</span>
                 
         </footer>
     </div>
