a059c047c9d536d01840b15dffa1c02806345de0
Rachit Sharma <98216013+rachit0304@users.noreply.github.com>
2024-07-03 15:50:54 +0530

    Update Home.jsx

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,98 +1,52 @@
-import React, { useEffect, useState } from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import Card from "../components/Card";
-import Loader from "../components/Loader";
-import Caraousel from "../components/Caraousel";
-
-export default function Home() {
-
-  const [search , setSearch] = useState("");
-  let [foodCat, setFoodcat] = useState([]); // we use map function for looping over an array only , we cant loop over an object by using map fo that we use forin
-
-  const [fetching , setFetching] = useState(false);
-  
-  let [fooditem, setFooditem] = useState([]);
-
-  const loadData = async () => {
-    setFetching(true);
-    let response = await fetch("https://go-food-server.onrender.com/api/foodData", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      }
-      
-    }).then(response => response.json()).then(data => {
-      setFoodcat(data);
-      setFetching(false);
-    } ).catch(err => console.log("error"))
-    
- 
-};
-
-  useEffect(() => {
-    loadData();
- 
-  }, []); 
-
-  return (
-    <div>
+ <div>
       <div>
-        
         <Navbar search={search} setSearch={setSearch} />
       </div>
 
-  <Caraousel />
+      <Caraousel search={search} setSearch={setSearch} />
 
+      <div className="container justify-items-center lg:w-75 md:w-100">
+        {fetching && <Loader />}
 
-      <div className="container">
-        
-         {
-         fetching && <Loader/>
-           
-         }
-        
-      {
-        
-       !fetching && foodCat[1] && foodCat[1].map(data=>(
-   
-              <div className="row mb-3 mx-5">
-             
-                  <div key={data._id}  className="fs-3 m-3">
-                    {data.CategoryName}
-                   {/* { console.log(data.CategoryName)} */}
-                  </div>
+        {!fetching &&
+          foodCat[1] &&
+          foodCat[1].map((data) => (
+            <div className="row mb-3 me-5 container">
+              <div key={data._id} className="fs-3 m-3 container">
+                {data.CategoryName}
 
-                  <hr />
-
-                { 
-                  foodCat[0] && foodCat[0]
-                    .filter((item) => (item.CategoryName === data.CategoryName) && (item.name.toString().toLowerCase().includes(search.toString().toLocaleLowerCase())))
-                    .map((filterItems) => {
-                      return (
-                        <div key={filterItems._id} className="col-12 col-md-6 col-lg-3">
-                         <Card fooditem = {filterItems}
-                         options={filterItems.options[0]}
-                         
-                         />
-                        </div>
-                      );
-                    })
-                 } 
-
+              <hr />
               </div>
-             
-        ))
-          
-          }
+
+
+              {foodCat[0] &&
+                foodCat[0]
+                  .filter(
+                    (item) =>
+                      item.CategoryName === data.CategoryName &&
+                      item.name
+                        .toString()
+                        .toLowerCase()
+                        .includes(search.toString().toLocaleLowerCase())
+                  )
+                  .map((filterItems) => {
+                    return (
+                      <div
+                        key={filterItems._id}
+                        className="col-12 col-md-6 col-lg-3 ms-5 container" style={{width: "18rem"}}
+                      >
+                        <Card
+                          fooditem={filterItems}
+                          options={filterItems.options[0]}
+                        />
+                      </div>
+                    );
+                  })}
+            </div>
+          ))}
       </div>
 
       <div>
-       
         <Footer />
       </div>
-
     </div>
-  )
-}
-
