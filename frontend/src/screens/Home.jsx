6f5a1d4c0c8a675ec6038685ed26f8ed03250faa
Rachit Sharma <98216013+rachit0304@users.noreply.github.com>
2024-07-03 16:44:40 +0530

    Update Home.jsx

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,4 +1,39 @@
- <div>
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
+import Loader from "../components/Loader";
+import Caraousel from "../components/Caraousel";
+
+export default function Home() {
+  const [search, setSearch] = useState("");
+  let [foodCat, setFoodcat] = useState([]); // we use map function for looping over an array only , we cant loop over an object by using map fo that we use forin
+
+  let [fooditem, setFooditem] = useState([]);
+  const [fetching, setFetching] = useState(false);
+
+  const loadData = async () => {
+    setFetching(true);
+    let response = await fetch("http://localhost:5000/api/foodData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setFoodcat(data);
+        setFetching(false);
+      })
+      .catch((err) => console.log("error"));
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <div>
       <div>
         <Navbar search={search} setSearch={setSearch} />
       </div>
@@ -6,47 +41,49 @@
       <Caraousel search={search} setSearch={setSearch} />
 
       <div className="container justify-items-center lg:w-75 md:w-100">
-        {fetching && <Loader />}
+    {fetching && <Loader />}
 
-        {!fetching &&
-          foodCat[1] &&
-          foodCat[1].map((data) => (
-            <div className="row mb-3 me-5 container">
-              <div key={data._id} className="fs-3 m-3 container">
-                {data.CategoryName}
+    {!fetching &&
+      foodCat[1] &&
+      foodCat[1].map((data) => (
+        <div className="row mb-3 container">
+          <div key={data._id} className="fs-3 m-3">
+            {data.CategoryName}
+            <hr />
+          </div>
 
-              <hr />
-              </div>
+          {foodCat[0] &&
+            foodCat[0]
+              .filter(
+                (item) =>
+                  item.CategoryName === data.CategoryName &&
+                  item.name
+                    .toString()
+                    .toLowerCase()
+                    .includes(search.toString().toLocaleLowerCase())
+              )
+              .map((filterItems) => {
+                return (
+                  <div
+                    key={filterItems._id}
+                    className="col-12 col-md-12 col-sm-3 col-lg-12 mb-4 card-render"
+                    
+                  >
+                    <Card
+                      fooditem={filterItems}
+                      options={filterItems.options[0]}
+                    />
+                  </div>
+                );
+              })}
+        </div>
+      ))}
+</div>
 
-
-              {foodCat[0] &&
-                foodCat[0]
-                  .filter(
-                    (item) =>
-                      item.CategoryName === data.CategoryName &&
-                      item.name
-                        .toString()
-                        .toLowerCase()
-                        .includes(search.toString().toLocaleLowerCase())
-                  )
-                  .map((filterItems) => {
-                    return (
-                      <div
-                        key={filterItems._id}
-                        className="col-12 col-md-6 col-lg-3 ms-5 container" style={{width: "18rem"}}
-                      >
-                        <Card
-                          fooditem={filterItems}
-                          options={filterItems.options[0]}
-                        />
-                      </div>
-                    );
-                  })}
-            </div>
-          ))}
-      </div>
 
       <div>
         <Footer />
       </div>
     </div>
+  );
+}
